98b97e5c03f204fa4372a3be4a2c57ec2e00d907
Eduardo Querido <eduardo.querido@znc.com.br>
2019-10-24 15:20:23 -0300

    Criados arquivos para deploy no Heroku

--- pypro/settings.py
+++ pypro/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'o5t%^q8zh#opq*-uau*elgu5lj4sk2nbye%#(ng+v)achsovu#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
